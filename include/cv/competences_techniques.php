bf7bad18f27788bf730a5dc6c6132764ccd9055d
Lucas GIRARD <contact@lucas-girard.fr>
2025-04-24 14:29:47 +0200

    Initial commit

---
+++ include/cv/competences_techniques.php
@@ -0,0 +1,156 @@
+	<tr>
+		<?php
+			if (isset($_GET['page']) && $_GET['page'] == "competences_techniquesd")
+			{
+			?>
+			<!--
+
+			
+			débutant
+			intermediaire
+			confirmé
+			avancé
+			expert
+			-->
+		<td class="section">COMPETENCES TECHNIQUES</td>
+		<td>
+Languages de Programmation
+<ul>
+    <li> <!--<!--()-->--> XHTML</li>
+	<li> <!--()--> HTML</li>
+    <li> <!--()--> CSS</li>
+    <li> <!--()--> PHP</li>
+    <li> <!--()--> SQL</li>
+
+	<li> <!--()--> DHTML</li>
+	<li> <!--()--> C</li>
+	<li> <!--()--> C++</li>
+	<li> <!--()--> FLASH AS2/AS3</li>
+	<li> <!--()--> Javascript</li>
+</ul>
+Compétences liées aux développements
+<ul>
+    <li> <!--()--> Normes W3C</li>
+    <li> <!--()--> Optimisation des pages</li>
+    <li> <!--()--> Affiliation</li>
+	<li> <!--()--> Administration phpMyAdmin</li>
+	<li> <!--()--> Méthode d'analyse, de conception et de réalisation de systèmes d'informations informatisés : Merise</li>
+	<li> <!--()--> Utilisation d'un debugger : xdebug</li>
+</ul>	
+Connaissances de CMS
+<ul>
+	<li> <!--()--> Framework Symfony</li>
+	<li> <!--()--> OScommerce</li>
+</ul>	
+Administration serveur
+<ul>
+	<li> <!--()--> installation et configuration diverses (Apache, Active Directory, ...)</li>
+	<li> <!--()--> utilisation du protocole SSH</li>
+</ul>
+Utilisation Système d'exploitation (OS)
+<ul>	
+	<li> <!--()--> Windows (95, 98, NT4, XP 32 et 64 bits)</li>
+	<li> <!--()--> Linux (Damn Small Linux, KNOPPIX)</li>
+	
+	<li> <!--()--> Windows Serveur 2003</li>
+	<li> <!--()--> Linux Serveur (gentoo, debian)</li>
+	
+	<li> <!--()--> Dépannage : Ultimate Boot CD</li>
+</ul>
+Connaissances de logiciels de développement
+<ul>
+	<li> <!--()--> Adobe Dreamweaver</li>
+	<li> <!--()--> Microsoft Visual Studio</li>
+	<li> <!--()--> netbeans</li>
+	<li> <!--()--> notepadd++</li>
+	<li> <!--()--> wamp</li>
+	<li> <!--()--> xoopserver</li>
+	<li> <!--()--> Environnement LAMP (Linux, Apache, MySQL, Php) : Wamp et Xoopserver</li>
+</ul>	
+Connaissances de logiciels de graphisme
+<ul>
+	<li> <!--()--> Adobe Flash</li>
+	<li> <!--()--> Adobe Photoshop</li>
+	<li> <!--()--> GIMP</li>
+	<li> <!--()--> SwishMax</li>
+</ul>
+Connaissances de suite bureautique
+<ul>	
+	<li> <!--()--> Microsoft Office</li>
+	<li> <!--()--> OpenOffice</li>
+</ul>
+Connaissances de navigateurs internet	
+<ul>
+	<li> <!--()--> Firefox</li>
+	<li> <!--()--> Internet Explorer</li>
+	<li> <!--()--> Opera</li>
+	<li> <!--()--> Safari</li>
+</ul>	
+Langues
+<ul>
+	<li> <!--()--> anglais (Technique).</li>
+</ul>
+Autres	
+<ul>
+	<li>Utilisation d’un ordinateur personnel depuis l'âge de 10 ans.</li>
+	<li>Installation et configuration hardware et software de PC.</li>
+	<li>Donne régulièrement des conseils en hardware auprès de connaissance.</li>
+	<li>Dépannage hardware et software de PC, par téléphone, VOIP ou bien sur place.</li>
+	<li>Connaissance sur les architectures matériels (processeurs, cartes mères, ...)</li>
+</ul>
+			<a href="index.php?page=competences_techniques">Réduire</a>
+			<?php
+			}
+			else
+			{
+			?>
+		<td class="section">COMPETENCES TECHNIQUES<br />(principale)</td>
+		<td>
+Languages de Programmation
+<ul>
+    <li>XHTML</li>
+	<li>HTML</li>
+    <li>CSS</li>
+    <li>PHP</li>
+    <li>SQL</li>
+</ul>
+Compétences liées aux développements
+<ul>
+    <li>Normes W3C</li>
+    <li>Optimisation des pages</li>
+    <li>Affiliation</li>
+	<li>Administration phpMyAdmin</li>
+	<li>Méthode d'analyse, de conception et de réalisation de systèmes d'informations informatisés : Merise</li>
+	<li>Utilisation d'un debugger : xdebug</li>
+</ul>	
+
+Utilisation Système d'exploitation (OS)
+<ul>	
+	<li>Windows (95, 98, NT4, XP 32 et 64 bits)</li>
+	<li>Linux (Damn Small Linux, KNOPPIX)</li>
+	
+	<li>Windows Serveur 2003</li>
+	<li>Linux Serveur (gentoo, debian)</li>
+	
+	<li>Dépannage : Ultimate Boot CD</li>
+</ul>
+Connaissances de logiciels de développement
+<ul>
+	<li>Adobe Dreamweaver</li>
+	<li>Microsoft Visual Studio</li>
+	<li>netbeans</li>
+	<li>notepadd++</li>
+	<li>wamp</li>
+	<li>xoopserver</li>
+	<li>Environnement LAMP (Linux, Apache, MySQL, Php) : Wamp et Xoopserver</li>
+</ul>	
+Langues
+<ul>
+	<li>anglais (Technique).</li>
+</ul>
+<a href="index.php?page=competences_techniquesd">En savoir +</a>
+			<?php
+			}
+			?>
+		</td>
+	</tr>
